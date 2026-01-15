49482f16aab41fde0bfd6112a39b0fb43900d864
Waleed-1996 <katbehw@gmail.com>
2026-01-15 17:47:34 +0300

    Add Automated test and add jasmane framwork

---
+++ tests/moneyTest.js
@@ -0,0 +1,22 @@
+import { currencyFormat } from "../scripts/utils/money.js";
+console.log('converts cents into dollars');
+if(currencyFormat(2095)==='20.95'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+console.log('works with 0');
+
+if(currencyFormat(0)==='0.00'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+console.log('the code rounds up to the nearest cent')
+if(currencyFormat(2000.5) === '20.01'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
